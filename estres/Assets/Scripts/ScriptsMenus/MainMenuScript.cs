1453c7a49560224dd24d8c089c5f36eed01e2f2b
Naiara <naiara_igartua@euneiz.com>
2025-10-07 21:18:35 +0200

    Functional play button

---
+++ estres/Assets/Scripts/ScriptsMenus/MainMenuScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+
+    }
+    
+}
