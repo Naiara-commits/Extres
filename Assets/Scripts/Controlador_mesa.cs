b80d0408aeb0cc276a8a407dfb09cde2ad89bdc1
Oliver <oliver_herrero@euneiz.com>
2025-10-18 20:39:53 +0200

    El controlador cambia las mesas de la lista de mesas ocupadas y mesas libres dependiendo de si están o no ocupadas

---
+++ Assets/Scripts/Controlador_mesa.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Controlador_mesa : MonoBehaviour
+{
+    List<GameObject> listaMesasOcupadas = new List<GameObject>();
+    List<GameObject> listaMesasLibres = new List<GameObject>();
+    int mesasOcupadas = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D mesa)
+    {
+        if (mesa.gameObject.tag == "Player") // Si el jugador toca la mesa
+        {
+             IntentarSentarCliente();
+        }
+
+    }
+
+    void IntentarSentarCliente()
+    {
+        if (mesasOcupadas < 2) // Si la cantidad de mesas ocupadas es menor a 2
+        {
+            { 
+                GameObject mesa = listaMesasLibres[0]; // Coge la primera mesa libre de la lista
+                mesasOcupadas++; // Aumenta la cantidad de mesas ocupadas
+                listaMesasOcupadas.Add(mesa); // Agrega la mesa a la lista de mesas ocupadas
+                listaMesasLibres.Remove(mesa); // Quita la mesa de la lista de mesas libres
+
+
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update() // Se llama contastemente para poder cambiar las mesas a ocupadas o libres
+    {
+        var numero = Random.Range(0,100); // Genera un número aleatorio entre 0 y 100
+        if (numero > 70) // Si el número es mayor a 70
+        {
+            IntentarSentarCliente();
+        }
+    }
+}
